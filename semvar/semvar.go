b2f356f39b863b69e93e7526a8003a667a8e4a27
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2023-05-23 15:41:17 +0530

    add pseudo version number comparison

--- semvar/semvar.go
+++ semvar/semvar.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -40,16 +41,47 @@ func Compare(v1Str, v2Str string) (string, error) {
 	// check if patch version is just a number
 	p1 := getPatchDigit(v1Split[2])
 	p2 := getPatchDigit(v2Split[2])
-	if p1 == p2 {
-		if len(v1Str) > len(v2Str) {
-			return v1Str, nil
-		}
-		return v2Str, nil
-	}
 	if p1 > p2 {
 		return v1Str, nil
+	} else if p2 < p1 {
+		return v2Str, nil
 	}
-	return v2Str, nil
+	// Patch digit is same, need to compare pseudo-version numbers
+	// Format: baseVersionPrefix-timestamp-revisionIdentifier
+	// v0.0.0-20190607162005-6e2aefe19808
+	// v0.0.0-20190319203104-747b5cae041f
+	// v0.9.0-rc.1.0.20190509204259-4246269fb68e
+	// v0.9.0-beta.2
+
+	// Check if patch version is NOT a standard pseudo-version numeber using it's length i.e. 28
+	if len(v1Split[2]) != 28+len(fmt.Sprintf("%d", p1)) {
+		return "", fmt.Errorf("version [%s] is not standard pseudo-version number. Human intervention is required", v1Str)
+	}
+
+	if len(v2Split[2]) != 28+len(fmt.Sprintf("%d", p2)) {
+		return "", fmt.Errorf("version [%s] is not standard pseudo-version number. Human intervention is required", v2Str)
+	}
+
+	p1Timestamp := strings.Split(v1Split[2], "-")[1]
+	p2Timestamp := strings.Split(v2Split[2], "-")[1]
+
+	var p1Time, p2Time time.Time
+	var err error
+	p1Time, err = convertTimestamp(p1Timestamp)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse timestamp [%s] for version %s due to %s", p1Timestamp, v1Str, err.Error())
+	}
+	p2Time, err = convertTimestamp(p2Timestamp)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse timestamp [%s] for version %s due to %s", p2Timestamp, v2Str, err.Error())
+	}
+	if p1Time.Equal(p2Time) {
+		return "", fmt.Errorf("cannot choose between versions [%s] and [%s] as both have same timestamp in pseudo-version number. Human intervention is required", v1Str, v2Str)
+	}
+	if p1Time.Before(p2Time) {
+		return v2Str, nil
+	}
+	return v1Str, nil
 }
 
 func getPatchDigit(pStr string) int {
@@ -67,4 +99,10 @@ func getPatchDigit(pStr string) int {
 	}
 	p, _ = strconv.Atoi(pStr[:index])
 	return p
+}
+
+func convertTimestamp(timestamp string) (time.Time, error) {
+	// Format: yymmddhhmmss
+	layout := "060102150405"
+	return time.Parse(layout, timestamp)
 }
